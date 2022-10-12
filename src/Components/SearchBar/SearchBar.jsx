0d4074b8ee0f8c8e39518aea4ca644bd076179ac
unknown <colost72@gmail.com>
2022-10-11 23:17:45 -0600

    Finished Search Bar Component

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 
 const SearchBar = (props) => {
-    return (
-        <div>
 
-        </div>
+const [searchTerm, setSearchTerm] = useState('');
+
+    return (
+        <form onSubmit={searchSongs}>
+            <input placeholder='Search Movies' type='text' value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)}/>
+            <button type='submit' >Search</button>
+        </form>>
     );
 }
 
